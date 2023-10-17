3d43d923f847e14c993cb83f2248a63ef42e211a
Chandni Gupta <125583314+nainagupta4571@users.noreply.github.com>
2023-10-18 00:23:42 +0530

    Create chandni2(fibonacci number).cpp

---
+++ chandni2(fibonacci number).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int fib0 = 0;
+        int fib1 = 1;
+        int fibn = 0;
+
+        for (int i = 2; i <= n; i++) {
+            fibn = fib0 + fib1;
+            fib0 = fib1;
+            fib1 = fibn;
+        }
+
+        return fibn;
+    }
+};
